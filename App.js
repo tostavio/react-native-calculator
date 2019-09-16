e21acdd28de6af6c34b3cfc3b85d3dbea6dec058
Octavio Tosta <octaviotosta.ti@gmail.com>
2019-09-16 17:51:04 -0300

    Aula 40

--- App.js
+++ App.js
@@ -1,114 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import Button from './src/components/Button'
+import Display from './src/components/Display'
+export default class App extends Component {
+  state = {
+    displayValue: '0',
+  }
+  addDigit = n => {
+    this.setState({displayValue : n})
+  }
+  clearMemory = () => {
+    this.setState({displayValue: '0'})
+  }
+  setOperation = operation => {
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue}></Display>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory} ></Button>
+          <Button label='/' operation onClick={this.setOperation}></Button>
+          <Button label='7' onClick={this.addDigit}></Button>
+          <Button label='8' onClick={this.addDigit}></Button>
+          <Button label='9' onClick={this.addDigit}></Button>
+          <Button label='*' operation onClick={this.setOperation}></Button>
+          <Button label='4' onClick={this.addDigit}></Button>
+          <Button label='5' onClick={this.addDigit}></Button>
+          <Button label='6' onClick={this.addDigit}></Button>
+          <Button label='-' operation onClick={this.setOperation}></Button>
+          <Button label='1' onClick={this.addDigit}></Button>
+          <Button label='2' onClick={this.addDigit}></Button>
+          <Button label='3' onClick={this.addDigit}></Button>
+          <Button label='+' operation onClick={this.setOperation}></Button>
+          <Button label='0' double onClick={this.addDigit}></Button>
+          <Button label='.' onClick={this.addDigit}></Button>
+          <Button label='=' operation onClick={this.setOperation}></Button>
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
+})
